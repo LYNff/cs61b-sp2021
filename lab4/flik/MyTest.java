a6671e784b9fab821efcb838eeae35f21c345741
LYN <2422040605@hhu.edu.cn>
2025-10-15 23:11:23 +0800

    lab4 debugging finished

---
+++ lab4/flik/MyTest.java
@@ -0,0 +1,11 @@
+package flik;
+
+import org.junit.Test;
+
+public class MyTest {
+    @Test
+    public void test() {
+        boolean sameNumber = Flik.isSameNumber(128, 128);
+        System.out.println(sameNumber);
+    }
+}
